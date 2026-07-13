41846a3369171bdcaccac3e07f19a2388efb6ca2
JoaoBoscoDuarte <joaoboscoduartef@gmail.com>
2026-07-13 00:27:48 -0300

    >

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -1,4 +1,4 @@
-from .loader import load_parquet, load_evolucao_acervo
+from .loader import load_parquet, load_evolucao_acervo, load_inclusoes_em_pauta, load_inclusoes_com_pauta
 from .filters import (
     filter_by_date_range,
     filter_by_values,
@@ -9,6 +9,8 @@ from .filters import (
 __all__ = [
     "load_parquet",
     "load_evolucao_acervo",
+    "load_inclusoes_em_pauta",
+    "load_inclusoes_com_pauta",
     "filter_by_date_range",
     "filter_by_values",
     "filter_by_text_search",
